08cb34af72771e099b343615404e95147ee971b5
Minsoda <dg4res@gmail.com>
2022-03-28 11:10:52 +0900

    삽입정렬 C++, C# 구현 완료

---
+++ insertion_sort/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N;
+vector<int> x;
+
+void print() {
+	for (int i = 0; i < N; i++) {
+		cout << x[i] << ' ';
+	}
+	cout << '\n';
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	freopen("input.txt", "r", stdin);
+
+	cin >> N;
+
+	x.resize(N);
+
+	for (int i = 0; i < N; i++) {
+		cin >> x[i];
+	}
+
+	cout << "정렬 전 : ";
+	print();
+	int i, j;
+
+	for (i = 1; i < N; i++) {
+		int box = x[i];
+		for (j = i - 1; j >= 0; j--) {
+			if (box > x[j]) {
+				break;
+			}
+			x[j + 1] = x[j];
+		}
+		x[j + 1] = box;
+	}
+
+	cout << "정렬 후 : ";
+	print();
+	return 0;
+}
---
+++ selection_sort_CSharp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace selection_sort_CSharp
+{
+    internal class Program
+    {
+        static void print(int[] x)
+        {
+            for (int i = 0; i < x.Length; i++)
+            {
+                Console.Write(x[i].ToString("D") + ' ');
+            }
+            Console.Write('\n');
+        }
+        static void Main(string[] args)
+        {
+            int[] x = { 2, 5, 1, 4, 6, 7, 8, 3, 9, 10 };
+
+            int i, j;
+
+            Console.Write("정렬 전: ");
+            print(x);
+
+            for (i = 1; i < x.Length; i++)
+            {
+                int box = x[i];
+                for (j = i - 1; j >= 0; j--)
+                {
+                    if (box > x[j])
+                    {
+                        break;
+                    }
+                    x[j + 1] = x[j];
+                }
+                x[j + 1] = box;
+            }
+
+            Console.Write("정렬 후: ");
+            print(x);
+        }
+    }
+}
